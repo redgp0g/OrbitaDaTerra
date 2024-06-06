70015e3fb5ab12dce76dbc76227deee73cc2b18a
redgp0g <guilhermeg2004@gmail.com>
2024-06-05 23:04:14 -0300

    :sparkles: feat Adicionar Views de Home - Criar view index - Criar view contempladas - Criar partial de navbar

---
+++ resources/views/home/contempladas.blade.php
@@ -0,0 +1,218 @@
+@extends('master')
+@section('title', 'Cartas Contempladas')
+@section('conteudo')
+    @php $cartas = json_decode(file_get_contents('https://fragaebitelloconsorcios.com.br/api/json/contemplados'), true); @endphp
+
+    @include('partials.navbarHome',['title' => 'Cartas Contempladas'])
+
+    <div class="section 3" style="min-height: 85vh;">
+        <div class="container">
+            <div class="d-sm-flex py-3 align-items-sm-center">
+                <div class="my-3 d-sm-flex">
+                    <label>Categoria:</label>
+                    <select id="filtroCategoria" class="ms-1 form-select form-select-sm w-auto"></select>
+                </div>
+                <div class="my-3 ms-2 d-sm-flex">
+                    <label>Disponível:</label>
+                    <select id="filtroDisponibilidade" class="ms-1 form-select form-select-sm w-auto"></select>
+                </div>
+                <div class="my-3 ms-2 d-sm-flex">
+                    <label>Administradora:</label>
+                    <select id="filtroAdministradora" class="ms-1 form-select form-select-sm w-auto"></select>
+                </div>
+                <div class="ps-5 h-50 d-sm-flex">
+                    <button class="btn btn-info" id="limparFiltros">Limpar Filtros</button>
+                </div>
+            </div>
+            <table id="table" class="table table-striped">
+                <thead>
+                    <tr>
+                        <th>Código</th>
+                        <th>Categoria</th>
+                        <th>Crédito</th>
+                        <th>Entrada</th>
+                        <th>Meses</th>
+                        <th>Parcela</th>
+                        <th>Taxa</th>
+                        <th>Administradora</th>
+                        <th>Disponibilidade</th>
+                        <th>Ações</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($cartas as $carta)
+                        @php
+                            $saldoDevedor = $carta['valor_credito'] - $carta['entrada'];
+                            $parcelas = $carta['parcelas'];
+                            $totalParcelas = $carta['valor_parcela'] * $parcelas;
+                            $percentualParcela = (($totalParcelas - $saldoDevedor) / $saldoDevedor / $parcelas) * 100;
+                        @endphp
+                        <tr>
+                            <td>{{ htmlspecialchars($carta['id']) }}</td>
+                            <td>{{ htmlspecialchars($carta['categoria']) }}</td>
+                            <td>R${{ number_format($carta['valor_credito'], 2, ',', '.') }}</td>
+                            <td>R${{ number_format($carta['entrada'], 2, ',', '.') }}</td>
+                            <td>{{ htmlspecialchars($carta['parcelas']) }}</td>
+                            <td>R${{ number_format($carta['valor_parcela'], 2, ',', '.') }}</td>
+                            <td>{{ number_format($percentualParcela, 2, ',') }}%</td>
+                            <td>{{ htmlspecialchars($carta['administradora']) }}</td>
+                            <td>
+                                @if ($carta['reserva'] == 'Reservar')
+                                    Disponível
+                                @else
+                                    Reservado
+                                @endif
+                            </td>
+                            <td>
+                                @if ($carta['reserva'] == 'Reservar')
+                                    <button class="btn btn-success reservar" data-carta-id="{{ $carta['id'] }}"
+                                        data-carta-categoria="{{ $carta['categoria'] }}"
+                                        data-carta-valor="{{ $carta['valor_credito'] }}"
+                                        onclick="reservarCarta(this)">Reservar</button>
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
+
+    <dialog id="dialogFaleVendedor" class="rounded rounded-4 border border-0 p-5" style="z-index: 100;">
+        <button style="right: 10px; top:10px" class="position-absolute" id="fecharDialog"
+            onclick="dialogFaleVendedor.close()"><i class="fa fa-times"></i></button>
+        <h2 class="mb-2">Preencha para conversar com um Vendedor</h2>
+        <h4 class="text-center text-danger fs-6">Os campos com * são obrigatórios</h4>
+        <form id="enviarDados" method="post" action="/public/index.php/enviardados">
+            <div class="mb-3">
+                <label for="Nome" class="form-label">Nome <span class="text-danger">*</span></label>
+                <input type="text" class="form-control" id="Nome" name="Nome" placeholder="Nome" autofocus
+                    required />
+            </div>
+            <div class="mb-3">
+                <label for="celular" class="form-label">Celular <span class="text-danger">*</span></label>
+                <input type="text" class="form-control celular" id="celular" name="Telefone"
+                    placeholder="Digite seu celular com DDD" maxlength="16" required pattern="\(\d{2}\) \d{5}-\d{4}" />
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">E-mail</label>
+                <input type="email" class="form-control email" name="Email" placeholder="Digite seu e-mail" />
+            </div>
+            <input type="hidden" name="numerovendedor" id="numerovendedor" value="<?php echo $cadastro->Telefone; ?>">
+            <button type="submit" id="btnCadastro" class="btn btn-primary d-grid w-100">Continuar</button>
+        </form>
+    </dialog>
+
+    <button class="float fs-1" target="_blank" id="btnmodal" onclick="dialogFaleVendedor.showModal()"><i
+            class="fa fa-whatsapp px-2"></i></button>
+
+    <script>
+        const dialogFaleVendedor = document.getElementById('dialogFaleVendedor');
+        const tiposCarta = document.querySelectorAll('.dropdown-menu li');
+        const nav = document.getElementById('navbarSupportedContent');
+
+        var filtroAdministradoraSelecionada = '';
+        var filtroCategoriaSelecionada = '';
+        var filtroDisponibilidadeSelecionada = '';
+
+        function atualizarOptionsfiltro(table, filtro, posicaoColuna, filtroSelecionado) {
+            filtro.empty();
+            filtro.append('<option value="">Todas</option>');
+
+            var dados = table.column(posicaoColuna, {
+                filter: 'applied'
+            }).data();
+
+            var dadosUnicos = dados.unique().toArray();
+
+            dadosUnicos.forEach(function(value) {
+                var count = dados.filter(function(item) {
+                    return item === value;
+                }).count();
+
+                if (value !== null && value !== '') {
+                    filtro.append('<option value="' + value + '"' + (filtroSelecionado === value ? ' selected' :
+                        '') + '>' + value + ' (' + count + ') </option>');
+                }
+            });
+        }
+
+        function reservarCarta(button) {
+            var numeroVendedor = $('#numerovendedor').val();
+            var mensagemPadrao = "Olá, eu gostaria de reservar uma carta contemplada! Código: " + button.dataset.cartaId +
+                ", valor de crédito: " + button.dataset.cartaValor + ", carta de " + button.dataset.cartaCategoria;
+
+            mensagemPadrao = encodeURIComponent(mensagemPadrao);
+
+            var linkWhatsApp = 'https://api.whatsapp.com/send?phone=' + numeroVendedor + '&text=' + mensagemPadrao;
+
+            window.location.href = linkWhatsApp;
+        }
+
+
+        $('.email').on('focusout', function() {
+            let email = this.val();
+            validarEmail(email, this);
+        });
+
+        $(document).ready(function() {
+            var table = $('#table').DataTable({
+                order: [],
+                "language": {
+                    "url": "https://cdn.datatables.net/plug-ins/1.13.4/i18n/pt-BR.json"
+                },
+                "dom": '<"row align-items-center"<"col-md-6" l><"col-md-6" f>>' +
+                    '<"table-responsive border-bottom my-3" rt><"row align-items-center"' +
+                    '<"col-md-6" i><"col-md-6" p>><"clear">',
+                "scrollY": "60vh",
+                "scrollX": true,
+                "scrollCollapse": true,
+                "autoWidth": true,
+                "responsive": true,
+            });
+
+            atualizarFiltros();
+
+            $('#filtroCategoria').on('change', function() {
+                var filtro = $(this).val();
+                filtroCategoriaSelecionada = filtro;
+                table.column(1).search(filtro).draw();
+
+                atualizarFiltros();
+            });
+
+            $('#filtroAdministradora').on('change', function() {
+                var filtro = $(this).val();
+                filtroAdministradoraSelecionada = filtro;
+                table.column(7).search(filtro).draw();
+
+                atualizarFiltros();
+            });
+
+            $('#filtroDisponibilidade').on('change', function() {
+                var filtro = $(this).val();
+                filtroDisponibilidadeSelecionada = filtro;
+                table.column(8).search(filtro).draw();
+
+                atualizarFiltros();
+            });
+
+            function atualizarFiltros() {
+                atualizarOptionsfiltro(table, $('#filtroCategoria'), 1, filtroCategoriaSelecionada);
+                atualizarOptionsfiltro(table, $('#filtroAdministradora'), 7, filtroAdministradoraSelecionada);
+                atualizarOptionsfiltro(table, $('#filtroDisponibilidade'), 8, filtroDisponibilidadeSelecionada);
+            }
+
+            $('#limparFiltros').on('click', function() {
+                table.column(1).search('').draw();
+                table.column(7).search('').draw();
+                table.column(8).search('').draw();
+                filtroDisponibilidadeSelecionada = '';
+                filtroAdministradoraSelecionada = '';
+                filtroCategoriaSelecionada = '';
+                atualizarFiltros();
+            });
+
+        });
+    </script>
+@endsection
---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,124 @@
+@extends('master')
+@section('title', 'Cartas Novas')
+@section('conteudo')
+    @include('partials.navbarHome', ['title' => 'Cartas Novas'])
+
+    <div class="section 3" style="min-height: 85vh;">
+        <div class="container">
+            <div class="row">
+                @for ($x = 1; $x <= 7; $x++)
+                    @php
+                        $filteredCartas = $cartas->filter(function ($carta) use ($x) {
+                            return $carta->IDTipoCarta == $x;
+                        });
+
+                        $sortedCartas = $filteredCartas->sort(function ($a, $b) {
+                            if ($a->Prazo != $b->Prazo) {
+                                return $b->Prazo - $a->Prazo;
+                            }
+
+                            return $b->ValorCredito - $a->ValorCredito;
+                        });
+                    @endphp
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="items">
+                                @foreach ($sortedCartas as $carta)
+                                    <div class="card">
+                                        <img src="{{ asset('/images/tipoproduto/' . $carta->tipocarta->Imagem) }}">
+                                        <div class="card-body">
+                                            <h5 class="card-title">Consórcio de {{ $carta->Descricao }}</h5>
+                                            <p class="card-text"><span class="text-danger">Crédito:</span>
+                                                R${{ number_format($carta->ValorCredito, 2, ',', '.') }}</p>
+                                            <p class="card-text"><span class="text-danger">Parcela Integral:</span>
+                                                R${{ number_format($carta->ParcelaIntegral, 2, ',', '.') }}</p>
+                                            <p class="card-text"><span class="text-danger">Parcela Flex
+                                                    ({{ number_format($carta->PercentualFlex) }}%)
+                                                    : </span>
+                                                R${{ number_format($carta->ParcelaFlex, 2, ',', '.') }}</p>
+                                            <p class="card-text"><span class="text-danger">Prazo:</span> {{ $carta->Prazo }}
+                                                Meses</p>
+                                        </div>
+                                    </div>
+                                @endforeach
+                            </div>
+                        </div>
+                    </div>
+                @endfor
+            </div>
+        </div>
+
+    </div>
+
+    <dialog id="dialogFaleVendedor" class="rounded rounded-4 border border-0 p-5" style="z-index: 100;">
+        <button style="right: 10px; top:10px" class="position-absolute" id="fecharDialog"
+            onclick="dialogFaleVendedor.close()"><i class="fa fa-times"></i></button>
+        <h2 class="mb-2">Preencha para conversar com um Vendedor</h2>
+        <h4 class="text-center text-danger fs-6">Os campos com * são obrigatórios</h4>
+        <form id="enviarDados" method="post" action="/public/index.php/enviardados">
+            <div class="mb-3">
+                <label for="Nome" class="form-label">Nome <span class="text-danger">*</span></label>
+                <input type="text" class="form-control" id="Nome" name="Nome" placeholder="Nome" autofocus
+                    required />
+            </div>
+            <div class="mb-3">
+                <label for="celular" class="form-label">Celular <span class="text-danger">*</span></label>
+                <input type="text" class="form-control celular" id="celular" name="Telefone"
+                    placeholder="Digite seu celular com DDD" maxlength="16" required pattern="\(\d{2}\) \d{5}-\d{4}" />
+            </div>
+            <div class="mb-3">
+                <label for="email" class="form-label">E-mail</label>
+                <input type="email" class="form-control email" name="Email" placeholder="Digite seu e-mail" />
+            </div>
+            <input type="hidden" name="numerovendedor" id="numerovendedor" value="<?php echo $cadastro->Telefone; ?>">
+            <button type="submit" id="btnCadastro" class="btn btn-primary d-grid w-100">Continuar</button>
+        </form>
+    </dialog>
+
+    <button class="float fs-1" target="_blank" id="btnmodal" onclick="dialogFaleVendedor.showModal()"><i
+            class="fa fa-whatsapp px-2"></i></button>
+
+    <script>
+        const dialogFaleVendedor = document.getElementById('dialogFaleVendedor');
+
+        $(document).ready(function() {
+
+            $('.items').slick({
+                infinite: true,
+                slidesToShow: 5,
+                slidesToScroll: 1,
+                autoplay: true,
+                autoplaySpeed: 2000,
+                responsive: [{
+                        breakpoint: 991,
+                        settings: {
+                            slidesToShow: 3,
+                            slidesToScroll: 1,
+                        }
+                    },
+                    {
+                        breakpoint: 700,
+                        settings: {
+                            arrows: false,
+                            slidesToShow: 3,
+                            slidesToScroll: 1,
+                        }
+                    },
+                    {
+                        breakpoint: 520,
+                        settings: {
+                            arrows: false,
+                            slidesToShow: 2,
+                            slidesToScroll: 1,
+                        }
+                    },
+                ]
+            });
+        });
+
+        $('.email').on('focusout', function() {
+            let email = this.val();
+            validarEmail(email, this);
+        });
+    </script>
+@endsection
