250ad263e0b7f6a926a7e0cd4fef6f5577726d01
redgp0g <guilhermeg2004@gmail.com>
2024-06-05 23:07:57 -0300

    :heavy_plus_sign: Criar partial com Links e Scripts Dashboard UI

---
+++ resources/views/partials/dashboardLinks.blade.php
@@ -0,0 +1,20 @@
+<!-- Library / Plugin Css Build -->
+<link rel="stylesheet" href="{{ asset('dashboard/css/core/libs.min.css') }}">
+
+<!-- Aos Animation Css -->
+<link rel="stylesheet" href="{{ asset('dashboard/vendor/aos/dist/aos.css') }}">
+
+<!-- Hope Ui Design System Css -->
+<link rel="stylesheet" href="{{ asset('dashboard/css/hope-ui.min.css?v=4.0.0') }}">
+
+<!-- Custom Css -->
+<link rel="stylesheet" href="{{ asset('dashboard/css/custom.min.css?v=4.0.0') }}">
+
+<!-- Dark Css -->
+<link rel="stylesheet" href="{{ asset('dashboard/css/dark.min.css') }}">
+
+<!-- Customizer Css -->
+<link rel="stylesheet" href="{{ asset('dashboard/css/customizer.min.css') }}">
+
+<!-- RTL Css -->
+<link rel="stylesheet" href="{{ asset('dashboard/css/rtl.min.css') }}">
---
+++ resources/views/partials/dashboardScripts.blade.php
@@ -0,0 +1,29 @@
+<!-- Library Bundle Script -->
+<script src="{{ asset('dashboard/js/core/libs.min.js') }}"></script>
+
+<!-- External Library Bundle Script -->
+<script src="{{ asset('dashboard/js/core/external.min.js') }}"></script>
+
+<!-- Widgetchart Script -->
+<script src="{{ asset('dashboard/js/charts/widgetcharts.js') }}"></script>
+
+<!-- mapchart Script -->
+<script src="{{ asset('dashboard/js/charts/vectore-chart.js') }}"></script>
+<script src="{{ asset('dashboard/js/charts/dashboard.js') }}"></script>
+
+<!-- fslightbox Script -->
+<script src="{{ asset('dashboard/js/plugins/fslightbox.js') }}"></script>
+
+<!-- Settings Script -->
+<script src="{{ asset('dashboard/js/plugins/setting.js') }}"></script>
+
+<!-- Slider-tab Script -->
+<script src="{{ asset('dashboard/js/plugins/slider-tabs.js') }}"></script>
+
+<!-- Form Wizard Script -->
+<script src="{{ asset('dashboard/js/plugins/form-wizard.js') }}"></script>
+
+<!-- AOS Animation Plugin-->
+<script src="{{ asset('dashboard/vendor/aos/dist/aos.js') }}"></script>
+<!-- App Script -->
+<script src="{{ asset('dashboard/js/hope-ui.js') }}" defer></script>
