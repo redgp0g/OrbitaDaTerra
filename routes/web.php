303c052a3a5c33c9563269e07a7e3f8e0b6a8936
redgp0g <guilhermeg2004@gmail.com>
2024-06-05 23:06:42 -0300

    :sparkles: feat Configurar Rotas - Criar rota para Index e Contempladas de HomeController

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,9 @@
 <?php
 
+use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/{idVendedor?}', [HomeController::class, 'index']);
+Route::get('/contempladas/{idVendedor?}', [HomeController::class, 'contempladas']);
 
-Route::get('/', function () {
-    return view('welcome');
 });
