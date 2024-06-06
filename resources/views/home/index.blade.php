d181478affebb348ce7cbbf4e8d4f648f116aca9
redgp0g <guilhermeg2004@gmail.com>
2024-06-05 23:26:39 -0300

    :recycle: refactor Criar componente Fale com o Vendedor - Atualizar views Contempladas e Index com novo componente

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -58,40 +58,10 @@
         </div>
 
     </div>
-
-    <dialog id="dialogFaleVendedor" class="rounded rounded-4 border border-0 p-5" style="z-index: 100;">
-        <button style="right: 10px; top:10px" class="position-absolute" id="fecharDialog"
-            onclick="dialogFaleVendedor.close()"><i class="fa fa-times"></i></button>
-        <h2 class="mb-2">Preencha para conversar com um Vendedor</h2>
-        <h4 class="text-center text-danger fs-6">Os campos com * são obrigatórios</h4>
-        <form id="enviarDados" method="post" action="/public/index.php/enviardados">
-            <div class="mb-3">
-                <label for="Nome" class="form-label">Nome <span class="text-danger">*</span></label>
-                <input type="text" class="form-control" id="Nome" name="Nome" placeholder="Nome" autofocus
-                    required />
-            </div>
-            <div class="mb-3">
-                <label for="celular" class="form-label">Celular <span class="text-danger">*</span></label>
-                <input type="text" class="form-control celular" id="celular" name="Telefone"
-                    placeholder="Digite seu celular com DDD" maxlength="16" required pattern="\(\d{2}\) \d{5}-\d{4}" />
-            </div>
-            <div class="mb-3">
-                <label for="email" class="form-label">E-mail</label>
-                <input type="email" class="form-control email" name="Email" placeholder="Digite seu e-mail" />
-            </div>
-            <input type="hidden" name="numerovendedor" id="numerovendedor" value="<?php echo $cadastro->Telefone; ?>">
-            <button type="submit" id="btnCadastro" class="btn btn-primary d-grid w-100">Continuar</button>
-        </form>
-    </dialog>
-
-    <button class="float fs-1" target="_blank" id="btnmodal" onclick="dialogFaleVendedor.showModal()"><i
-            class="fa fa-whatsapp px-2"></i></button>
+    @include('components.faleComOVendedor', ['cadastro' => $cadastro])
 
     <script>
-        const dialogFaleVendedor = document.getElementById('dialogFaleVendedor');
-
         $(document).ready(function() {
-
             $('.items').slick({
                 infinite: true,
                 slidesToShow: 5,
@@ -125,9 +95,5 @@
             });
         });
 
-        $('.email').on('focusout', function() {
-            let email = this.val();
-            validarEmail(email, this);
-        });
     </script>
 @endsection
