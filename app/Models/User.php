f728cbf9a883561e50b670640bdeb0ede1e40cb8
redgp0g <guilhermeg2004@gmail.com>
2024-06-06 22:37:21 -0300

    Adaptar User para Usuario - Substituir colunas "email" e "password" por "Login" e "Senha", alterando os atributos da classe User - Trocar nome da tabela para "Usuario" - Sobrepor function de pegar senha do usuário - Sobrepor PK para "IDUsuario"

--- app/Models/User.php
+++ app/Models/User.php
@@ -12,33 +12,28 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
+    
+    protected $table = 'Usuario';
+    protected $primaryKey = 'IDUsuario';
+
     protected $fillable = [
-        'name',
-        'email',
-        'password',
+        'Login',
+        'Senha',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var array<int, string>
-     */
     protected $hidden = [
-        'password',
+        'Senha',
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
+    
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    
+    public function getAuthPassword()
+    {
+        return $this->Senha;
+    }
 }
