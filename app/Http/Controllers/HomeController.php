20f78e22481856a572d1f848281ff7b8d488b839
redgp0g <guilhermeg2004@gmail.com>
2024-06-15 11:24:34 -0300

    Configurar HomeController - Corrigir possível cadastro nulo - Adicionar fucntion simulacao

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -10,24 +10,24 @@ class HomeController extends Controller
 {
     public function index($idVendedor = 38)
     {
-        $cadastro = Cadastro::find($idVendedor);
-
-        if (!$cadastro) {
-            $cadastro = Cadastro::find(38);
-        }
+        $cadastro = Cadastro::find($idVendedor) ?? Cadastro::find(38);
         $cartas = Carta::with('tipocarta')->get();
-        return view('home/index', compact('cadastro','cartas'));
+
+        return view('home/index', compact('cadastro', 'cartas'));
     }
-    
+
     public function contempladas($idVendedor = 38)
     {
-        $cadastro = Cadastro::find($idVendedor);
-
-        if (!$cadastro) {
-            $cadastro = Cadastro::find(38);
-        }
+        $cadastro = Cadastro::find($idVendedor) ?? Cadastro::find(38);
 
         return view('home/contempladas', compact('cadastro'));
     }
 
+    public function simulacao($idVendedor = 38)
+    {
+        $cadastro = Cadastro::find($idVendedor) ?? Cadastro::find(38);
+        $tiposCarta = TipoCarta::all();
+
+        return view('home/simulacao', compact('cadastro','tiposCarta'));
+    }
 }
