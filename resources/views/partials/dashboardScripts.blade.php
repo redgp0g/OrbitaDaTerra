8823610b9ffc18be4ccf585190b040cc648fcf00
redgp0g <guilhermeg2004@gmail.com>
2024-06-15 11:23:12 -0300

    :truck: Renomear pasta de assets UI-Dashboard

--- resources/views/partials/dashboardScripts.blade.php
+++ resources/views/partials/dashboardScripts.blade.php
@@ -1,29 +1,29 @@
 <!-- Library Bundle Script -->
-<script src="{{ asset('dashboard/js/core/libs.min.js') }}"></script>
+<script src="{{ asset('ui-assets/js/core/libs.min.js') }}"></script>
 
 <!-- External Library Bundle Script -->
-<script src="{{ asset('dashboard/js/core/external.min.js') }}"></script>
+<script src="{{ asset('ui-assets/js/core/external.min.js') }}"></script>
 
 <!-- Widgetchart Script -->
-<script src="{{ asset('dashboard/js/charts/widgetcharts.js') }}"></script>
+<script src="{{ asset('ui-assets/js/charts/widgetcharts.js') }}"></script>
 
 <!-- mapchart Script -->
-<script src="{{ asset('dashboard/js/charts/vectore-chart.js') }}"></script>
-<script src="{{ asset('dashboard/js/charts/dashboard.js') }}"></script>
+<script src="{{ asset('ui-assets/js/charts/vectore-chart.js') }}"></script>
+<script src="{{ asset('ui-assets/js/charts/dashboard.js') }}"></script>
 
 <!-- fslightbox Script -->
-<script src="{{ asset('dashboard/js/plugins/fslightbox.js') }}"></script>
+<script src="{{ asset('ui-assets/js/plugins/fslightbox.js') }}"></script>
 
 <!-- Settings Script -->
-<script src="{{ asset('dashboard/js/plugins/setting.js') }}"></script>
+<script src="{{ asset('ui-assets/js/plugins/setting.js') }}"></script>
 
 <!-- Slider-tab Script -->
-<script src="{{ asset('dashboard/js/plugins/slider-tabs.js') }}"></script>
+<script src="{{ asset('ui-assets/js/plugins/slider-tabs.js') }}"></script>
 
 <!-- Form Wizard Script -->
-<script src="{{ asset('dashboard/js/plugins/form-wizard.js') }}"></script>
+<script src="{{ asset('ui-assets/js/plugins/form-wizard.js') }}"></script>
 
 <!-- AOS Animation Plugin-->
-<script src="{{ asset('dashboard/vendor/aos/dist/aos.js') }}"></script>
+<script src="{{ asset('ui-assets/vendor/aos/dist/aos.js') }}"></script>
 <!-- App Script -->
-<script src="{{ asset('dashboard/js/hope-ui.js') }}" defer></script>
+<script src="{{ asset('ui-assets/js/hope-ui.js') }}" defer></script>
