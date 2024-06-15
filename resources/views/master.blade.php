6b402c22cda83c106049c5da39709f76e6fabc70
redgp0g <guilhermeg2004@gmail.com>
2024-06-14 22:53:42 -0300

    :bug: fix Corrigir fechamento de tag script na master

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -62,7 +62,7 @@
 </footer>
 
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
-<script src="{{ asset('js/app.js') }}"><script>
+<script src="{{ asset('js/app.js') }}"></script>
 </body>
 
 </html>
