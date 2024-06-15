91d7176a2fe3efaa24e68791acd4eede0d678558
redgp0g <guilhermeg2004@gmail.com>
2024-06-14 22:54:08 -0300

    :sparkles: feat Criar component alert para avisos flutuantes

---
+++ resources/views/components/alert.blade.php
@@ -0,0 +1,4 @@
+<div class="alert alert-container alert-{{ $style }} alert-dismissible fade show" role="alert">
+    {{ $mensagem }}
+    <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+</div>
