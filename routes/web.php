e3616f199ae321550170feec8a4ae64d54b85ca5
redgp0g <guilhermeg2004@gmail.com>
2024-06-14 22:46:03 -0300

    :construction: Criar Rotas - Criar Rota home, simulação, contempladas, login, auth, dashboard, logout, cadastrar usuário, lançar usuario

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/{idVendedor?}', [HomeController::class, 'index']);
-Route::get('/contempladas/{idVendedor?}', [HomeController::class, 'contempladas']);
-
-});
+Route::get('/{idVendedor?}', [HomeController::class, 'index'])->where('idVendedor','[0-9]+');
+Route::get('/contempladas/{idVendedor?}', [HomeController::class, 'contempladas'])->where('idVendedor','[0-9]+');
+Route::get('/simulacao/{idVendedor?}', [HomeController::class, 'simulacao'])->where('idVendedor','[0-9]+');
+Route::view('/usuario', 'usuario.login')->name('usuario.login');
+Route::post('/auth', [UsuarioController::class, 'auth'])->name('usuario.auth');
+Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index')->middleware('auth');
+Route::get('/logout', [UsuarioController::class, 'logout'])->name('usuario.logout');
+Route::get('/usuario/create/{id?}', [UsuarioController::class, 'create'])->name('usuario.create')->where('id','[0-9]+');
+Route::post('/usuario/store', [UsuarioController::class, 'store'])->name('usuario.store');
