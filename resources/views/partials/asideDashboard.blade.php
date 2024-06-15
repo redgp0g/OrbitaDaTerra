e049785a8b36bbeaffdb0d1fbced4a4bb469931a
redgp0g <guilhermeg2004@gmail.com>
2024-06-14 22:58:40 -0300

    :bug: fix Corrigir caminhos dos assets Hope-UI

---
+++ resources/views/partials/asideDashboard.blade.php
@@ -0,0 +1,149 @@
+<aside class="sidebar sidebar-default sidebar-white sidebar-base navs-rounded-all sidebar-mini">
+    <div class="sidebar-header d-flex align-items-center justify-content-start">
+        <a href="/public/index.php/dashboard" class="navbar-brand">
+            <div class="logo-main">
+                <img src="{{asset('/images/logo mini.png')}}" height="40px">
+            </div>
+
+            <h4 class="logo-title">ÓRBITA</h4>
+        </a>
+        <div class="sidebar-toggle" data-toggle="sidebar" data-active="true">
+            <i class="icon">
+                <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M4.25 12.2744L19.25 12.2744" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                    <path d="M10.2998 18.2988L4.2498 12.2748L10.2998 6.24976" stroke="currentColor" stroke-width="1.5" stroke-linecap="round" stroke-linejoin="round"></path>
+                </svg>
+            </i>
+        </div>
+    </div>
+    <div class="sidebar-body pt-0 data-scrollbar">
+        <div class="sidebar-list">
+            <ul class="navbar-nav iq-main-menu" id="sidebar-menu">
+                <li class="nav-item">
+                    <a class="nav-link" data-bs-toggle="collapse" href="#sidebar-consultar" role="button" aria-expanded="false" aria-controls="sidebar-consultar">
+                        <i class="icon">
+                            <svg class="icon-20" width="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path opacity="0.4" d="M16.0756 2H19.4616C20.8639 2 22.0001 3.14585 22.0001 4.55996V7.97452C22.0001 9.38864 20.8639 10.5345 19.4616 10.5345H16.0756C14.6734 10.5345 13.5371 9.38864 13.5371 7.97452V4.55996C13.5371 3.14585 14.6734 2 16.0756 2Z" fill="currentColor"></path>
+                                <path fill-rule="evenodd" clip-rule="evenodd" d="M4.53852 2H7.92449C9.32676 2 10.463 3.14585 10.463 4.55996V7.97452C10.463 9.38864 9.32676 10.5345 7.92449 10.5345H4.53852C3.13626 10.5345 2 9.38864 2 7.97452V4.55996C2 3.14585 3.13626 2 4.53852 2ZM4.53852 13.4655H7.92449C9.32676 13.4655 10.463 14.6114 10.463 16.0255V19.44C10.463 20.8532 9.32676 22 7.92449 22H4.53852C3.13626 22 2 20.8532 2 19.44V16.0255C2 14.6114 3.13626 13.4655 4.53852 13.4655ZM19.4615 13.4655H16.0755C14.6732 13.4655 13.537 14.6114 13.537 16.0255V19.44C13.537 20.8532 14.6732 22 16.0755 22H19.4615C20.8637 22 22 20.8532 22 19.44V16.0255C22 14.6114 20.8637 13.4655 19.4615 13.4655Z" fill="currentColor"></path>
+                            </svg>
+                        </i>
+                        <span class="item-name">CONSULTAR</span>
+                        <i class="right-icon">
+                            <svg class="icon-18" xmlns="http://www.w3.org/2000/svg" width="18" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                            </svg>
+                        </i>
+                    </a>
+                    <ul class="sub-nav collapse" id="sidebar-consultar" data-bs-parent="#sidebar-menu">
+                        <li class="nav-item">
+                            <a class="nav-link " href="/public/index.php/dashboard">
+                                <i class="icon">
+                                    <svg class="icon-10" xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor">
+                                        <g>
+                                            <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                        </g>
+                                    </svg>
+                                </i>
+                                <i class="sidenav-mini-icon"> L </i>
+                                <span class="item-name">Meus Leads</span>
+                            </a>
+                        </li>
+                    </ul>
+                    
+                        <ul class="sub-nav collapse" id="sidebar-consultar" data-bs-parent="#sidebar-menu">
+                            <li class="nav-item">
+                                <a class="nav-link " href="/public/index.php/dashboard/contas">
+                                    <i class="icon">
+                                        <svg class="icon-10" xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor">
+                                            <g>
+                                                <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                            </g>
+                                        </svg>
+                                    </i>
+                                    <i class="sidenav-mini-icon"> C </i>
+                                    <span class="item-name">Contas</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="sub-nav collapse" id="sidebar-consultar" data-bs-parent="#sidebar-menu">
+                            <li class="nav-item">
+                                <a class="nav-link " href="/public/index.php/dashboard/historicoAcesso">
+                                    <i class="icon">
+                                        <svg class="icon-10" xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor">
+                                            <g>
+                                                <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                            </g>
+                                        </svg>
+                                    </i>
+                                    <i class="sidenav-mini-icon"> H </i>
+                                    <span class="item-name">Histórico de Acesso</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="sub-nav collapse" id="sidebar-consultar" data-bs-parent="#sidebar-menu">
+                            <li class="nav-item">
+                                <a class="nav-link " href="/public/index.php/dashboard/indexEmpresa">
+                                    <i class="icon">
+                                        <svg class="icon-10" xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor">
+                                            <g>
+                                                <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                            </g>
+                                        </svg>
+                                    </i>
+                                    <i class="sidenav-mini-icon"> E </i>
+                                    <span class="item-name">Empresas</span>
+                                </a>
+                            </li>
+                        </ul>
+                </li>
+                    <li class="nav-item">
+                        <a class="nav-link" data-bs-toggle="collapse" href="#sidebar-cadastrar" role="button" aria-expanded="false" aria-controls="sidebar-cadastrar">
+                            <i class="icon">
+                                <svg class="icon-18" width="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path opacity="0.4" d="M18.8088 9.021C18.3573 9.021 17.7592 9.011 17.0146 9.011C15.1987 9.011 13.7055 7.508 13.7055 5.675V2.459C13.7055 2.206 13.5026 2 13.253 2H7.96363C5.49517 2 3.5 4.026 3.5 6.509V17.284C3.5 19.889 5.59022 22 8.16958 22H16.0453C18.5058 22 20.5 19.987 20.5 17.502V9.471C20.5 9.217 20.298 9.012 20.0465 9.013C19.6247 9.016 19.1168 9.021 18.8088 9.021Z" fill="currentColor"></path>
+                                    <path opacity="0.4" d="M16.0842 2.56737C15.7852 2.25637 15.2632 2.47037 15.2632 2.90137V5.53837C15.2632 6.64437 16.1742 7.55437 17.2792 7.55437C17.9772 7.56237 18.9452 7.56437 19.7672 7.56237C20.1882 7.56137 20.4022 7.05837 20.1102 6.75437C19.0552 5.65737 17.1662 3.69137 16.0842 2.56737Z" fill="currentColor"></path>
+                                    <path d="M14.3672 12.2364H12.6392V10.5094C12.6392 10.0984 12.3062 9.7644 11.8952 9.7644C11.4842 9.7644 11.1502 10.0984 11.1502 10.5094V12.2364H9.4232C9.0122 12.2364 8.6792 12.5704 8.6792 12.9814C8.6792 13.3924 9.0122 13.7264 9.4232 13.7264H11.1502V15.4524C11.1502 15.8634 11.4842 16.1974 11.8952 16.1974C12.3062 16.1974 12.6392 15.8634 12.6392 15.4524V13.7264H14.3672C14.7782 13.7264 15.1122 13.3924 15.1122 12.9814C15.1122 12.5704 14.7782 12.2364 14.3672 12.2364Z" fill="currentColor"></path>
+                                </svg>
+                            </i>
+                            <span class="item-name">CADASTRAR</span>
+                            <i class="right-icon">
+                                <svg class="icon-18" xmlns="http://www.w3.org/2000/svg" width="18" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                    <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7" />
+                                </svg>
+                            </i>
+                        </a>
+                        <ul class="sub-nav collapse" id="sidebar-cadastrar" data-bs-parent="#sidebar-menu">
+                            <li class="nav-item">
+                                <a class="nav-link " href="/public/index.php/dashboard/createEmpresa">
+                                    <i class="icon">
+                                        <svg class="icon-10" xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor">
+                                            <g>
+                                                <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                            </g>
+                                        </svg>
+                                    </i>
+                                    <i class="sidenav-mini-icon"> E </i>
+                                    <span class="item-name">Empresa</span>
+                                </a>
+                            </li>
+                        </ul>
+                        <ul class="sub-nav collapse" id="sidebar-cadastrar" data-bs-parent="#sidebar-menu">
+                            <li class="nav-item">
+                                <a class="nav-link " href="/public/index.php/dashboard/createCartaVendida">
+                                    <i class="icon">
+                                        <svg class="icon-10" xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24" fill="currentColor">
+                                            <g>
+                                                <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                            </g>
+                                        </svg>
+                                    </i>
+                                    <i class="sidenav-mini-icon"> CV </i>
+                                    <span class="item-name">Carta Vendida</span>
+                                </a>
+                            </li>
+                        </ul>
+                    </li>
+            </ul>
+        </div>
+    </div>
+</aside>
--- resources/views/partials/dashboardLinks.blade.php
+++ resources/views/partials/dashboardLinks.blade.php
@@ -1,20 +1,20 @@
 <!-- Library / Plugin Css Build -->
-<link rel="stylesheet" href="{{ asset('dashboard/css/core/libs.min.css') }}">
+<link rel="stylesheet" href="{{ asset('ui-assets/css/core/libs.min.css') }}">
 
 <!-- Aos Animation Css -->
-<link rel="stylesheet" href="{{ asset('dashboard/vendor/aos/dist/aos.css') }}">
+<link rel="stylesheet" href="{{ asset('ui-assets/vendor/aos/dist/aos.css') }}">
 
 <!-- Hope Ui Design System Css -->
-<link rel="stylesheet" href="{{ asset('dashboard/css/hope-ui.min.css?v=4.0.0') }}">
+<link rel="stylesheet" href="{{ asset('ui-assets/css/hope-ui.min.css?v=4.0.0') }}">
 
 <!-- Custom Css -->
-<link rel="stylesheet" href="{{ asset('dashboard/css/custom.min.css?v=4.0.0') }}">
+<link rel="stylesheet" href="{{ asset('ui-assets/css/custom.min.css?v=4.0.0') }}">
 
 <!-- Dark Css -->
-<link rel="stylesheet" href="{{ asset('dashboard/css/dark.min.css') }}">
+<link rel="stylesheet" href="{{ asset('ui-assets/css/dark.min.css') }}">
 
 <!-- Customizer Css -->
-<link rel="stylesheet" href="{{ asset('dashboard/css/customizer.min.css') }}">
+<link rel="stylesheet" href="{{ asset('ui-assets/css/customizer.min.css') }}">
 
 <!-- RTL Css -->
-<link rel="stylesheet" href="{{ asset('dashboard/css/rtl.min.css') }}">
+<link rel="stylesheet" href="{{ asset('ui-assets/css/rtl.min.css') }}">
