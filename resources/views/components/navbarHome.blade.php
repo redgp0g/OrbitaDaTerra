5292a04f0d9380785e5fd0cc8dc2c9faf54a3cc1
redgp0g <guilhermeg2004@gmail.com>
2024-07-15 20:42:37 -0300

    :lipstick: fix Corrigir menu hamburguer - Criar classe para w-100 até large devices

--- resources/views/components/navbarHome.blade.php
+++ resources/views/components/navbarHome.blade.php
@@ -1,17 +1,17 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-white border-bottom border-primary border-3">
     <div class="container-fluid">
-        <a class="navbar-brand mx-5 mb-2" href="#">
-            <img src="{{ asset('images/logo.png') }}" height="60px">
-        </a>
-
-        <div class="w-100 d-flex justify-content-center align-items-center d-lg-none">
-            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
-                aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
+        <div class="d-flex w-lg-100 pe-4">
+            <a class="navbar-brand mx-5 mb-2" href="#">
+                <img src="{{ asset('images/logo.png') }}" height="60px">
+            </a>
+            <div class="w-100 d-flex justify-content-end align-items-center d-lg-none">
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+            </div>
         </div>
-
+        
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0 fs-6">
                 @foreach ($items as $item)
