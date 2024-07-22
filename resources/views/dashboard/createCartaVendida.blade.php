577df4cef9ef342b57b8d43d0605a6afac08312d
redgp0g <guilhermeg2004@gmail.com>
2024-07-22 20:31:33 -0300

    :lipstick: feat Melhorar Cadastrar Carta Vendida - Criar máscara de real - Adicionar indicação de campos obrigatórios - Diminuido tamanho das colunas das div - Removido campo de vendedor e status, deixado status como padrão e hidden - Valor de garantia não é obrigatório

--- resources/views/dashboard/createCartaVendida.blade.php
+++ resources/views/dashboard/createCartaVendida.blade.php
@@ -6,8 +6,9 @@
                 <form method="POST" action="{{ url('/dashboard/storeCartaVendida') }}">
                     @csrf
                     <div class="row">
+                        <p class="text-danger">Campos com * são obrigatórios</p>
                         <div class="form-group col-lg-2">
-                            <label class="form-label" for="IDEmpresaAdministradora">Administradora</label>
+                            <label class="form-label" for="IDEmpresaAdministradora">Administradora <span class="text-danger">*</span></label>
                             <select class="form-control" name="IDEmpresaAdministradora" id="IDEmpresaAdministradora"
                                 required>
                                 <option value="" selected disabled>Selecione a Administradora</option>
@@ -18,7 +19,7 @@
                             </select>
                         </div>
                         <div class="form-group col-lg-2">
-                            <label class="form-label" for="IDEmpresaAutorizada">Autorizada</label>
+                            <label class="form-label" for="IDEmpresaAutorizada">Autorizada <span class="text-danger">*</span></label>
                             <select class="form-control" name="IDEmpresaAutorizada" id="IDEmpresaAutorizada" required>
                                 <option value="" selected disabled>Selecione a Autorizada</option>
                                 @foreach ($autorizadas as $autorizada)
@@ -36,23 +37,7 @@
                             </select>
                         </div>
                         <div class="form-group col-lg-4">
-                            <label class="form-label" for="IDCadastroVendedor">Vendedor</label>
-                            <select class="form-control" name="IDCadastroVendedor" id="IDCadastroVendedor" required>
-                                <option value="" selected disabled>Selecione o Vendedor</option>
-                                @foreach ($cadastros as $cadastro)
-                                    <option value="{{ $cadastro->IDCadastro }}">{{ $cadastro->Nome }}</option>
-                                @endforeach
-                            </select>
-                        </div>
-                        <div class="form-group col-lg-4">
-                            <label class="form-label" for="Status">Status</label>
-                            <select class="form-control" name="Status" id="Status" required>
-                                <option value="" selected disabled>Selecione o Status</option>
-                                <option value="Cadastrada">Cadastrada</option>
-                            </select>
-                        </div>
-                        <div class="form-group col-lg-4">
-                            <label class="form-label" for="IDTipoCarta">Tipo de Carta</label>
+                            <label class="form-label" for="IDTipoCarta">Tipo de Carta <span class="text-danger">*</span></label>
                             <select class="form-control " id="IDTipoCarta" name="IDTipoCarta" required>
                                 <option value="">Selecione o Tipo de Carta</option>
                                 @foreach ($tiposCartas as $tipoCarta)
@@ -60,45 +45,44 @@
                                 @endforeach
                             </select>
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="ValorCredito">Valor do Crédito</label>
-                            <input type="text" id="ValorCredito" name="ValorCredito" class="form-control"
-                                placeholder="Valor do Crédito" required />
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="ValorCredito">Valor do Crédito <span class="text-danger">*</span></label>
+                            <input type="text" id="ValorCredito" name="ValorCredito" class="form-control" data-type="currency" placeholder="R$1.000,00" required/>
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="ValorPago">Valor Pago</label>
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="ValorPago">Valor Pago <span class="text-danger">*</span></label>
                             <input type="text" id="ValorPago" name="ValorPago" class="form-control"
-                                placeholder="Valor Pago" required />
+                            data-type="currency" placeholder="R$1.000,00" required />
 
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="ValorVenda">Valor de Venda</label>
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="ValorVenda">Valor de Venda <span class="text-danger">*</span></label>
                             <input type="text" id="ValorVenda" name="ValorVenda" class="form-control"
-                                placeholder="Valor de Venda" required />
+                            data-type="currency" placeholder="R$1.000,00" required />
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="SaldoDevedor">Saldo Devedor</label>
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="SaldoDevedor">Saldo Devedor <span class="text-danger">*</span></label>
                             <input type="text" name="SaldoDevedor" id="SaldoDevedor" class="form-control"
-                                placeholder="Saldo Devedor" required />
+                            data-type="currency" placeholder="R$1.000,00" required />
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="ParcelasPagas">Parcelas Pagas</label>
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="ParcelasPagas">Parcelas Pagas <span class="text-danger">*</span></label>
                             <input type="number" id="ParcelasPagas" name="ParcelasPagas" step="1" min="1"
                                 max="600" class="form-control" placeholder="Parcelas Pagas" required>
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="ParcelasPagar">Parcelas a Pagar</label>
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="ParcelasPagar">Parcelas a Pagar <span class="text-danger">*</span></label>
                             <input type="number" id="ParcelasPagar" name="ParcelasPagar" step="1" min="1"
                                 max="600" class="form-control" placeholder="Parcelas a Pagar">
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="ValorParcela">Valor Parcela</label>
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="ValorParcela">Valor Parcela <span class="text-danger">*</span></label>
                             <input type="text" id="ValorParcela" name="ValorParcela" class="form-control"
-                                placeholder="Valor Parcela" required />
+                            data-type="currency" placeholder="R$1.000,00" required />
 
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="DiaVencimento">Dia do Vencimento</label>
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="DiaVencimento">Dia do Vencimento <span class="text-danger">*</span></label>
                             <select id="DiaVencimento" name="DiaVencimento" class="form-control" required>
                                 <option value="" selected disabled>Selecione o dia</option>
                                 @for ($i = 1; $i <= 31; $i++)
@@ -106,28 +90,28 @@
                                 @endfor
                                 <select>
                         </div>
-                        <div class="form-group col-lg-3">
+                        <div class="form-group col-lg-2">
                             <label class="form-label" for="ValorGarantia">Valor de Garantia</label>
                             <input type="text" id="ValorGarantia" name="ValorGarantia" class="form-control"
-                                placeholder="Valor de Garantia" required />
+                            data-type="currency" placeholder="R$1.000,00" />
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="Grupo">Grupo</label>
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="Grupo">Grupo <span class="text-danger">*</span></label>
                             <input type="text" id="Grupo" name="Grupo" class="form-control"
                                 placeholder="Grupo" required>
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="Cota">Cota</label>
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="Cota">Cota <span class="text-danger">*</span></label>
                             <input type="text" id="Cota" name="Cota" class="form-control" placeholder="Cota"
                                 required>
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="TaxaTransferencia">Taxa de Transferência</label>
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="TaxaTransferencia">Taxa de Transferência <span class="text-danger">*</span></label>
                             <input type="text" id="TaxaTransferencia" name="TaxaTransferencia" class="form-control"
-                                placeholder="Taxa de Transferência" required>
+                            data-type="currency" placeholder="R$1.000,00" required>
                         </div>
-                        <div class="form-group col-lg-3">
-                            <label class="form-label" for="Contemplada">Contemplada?</label>
+                        <div class="form-group col-lg-2">
+                            <label class="form-label" for="Contemplada">Contemplada? <span class="text-danger">*</span></label>
                             <div class="row">
                                 <div>
                                     <label for="taxaTransferenciaSim">Sim</label>
@@ -140,6 +124,7 @@
                             </div>
                         </div>
                         <div class="form-group col-lg-12">
+                            <input type="hidden" name="Status" value="Cadastrada"/>
                             <button type="submit" class="btn btn-success">Cadastrar</button>
                         </div>
                     </div>
