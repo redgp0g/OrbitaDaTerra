57261319a9f38e76a70c0d2105cbbb0ef814ff59
redgp0g <guilhermeg2004@gmail.com>
2024-12-28 23:30:55 -0300

    ✨ Adicionar meta tags de SEO à página principal
    Incluídas as tags "description" e "keywords" para melhorar a otimização de mecanismos de busca (SEO). Isso ajuda a aumentar a visibilidade da página e atrair mais tráfego orgânico.

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -20,17 +20,21 @@
     })(window, document, 'script', 'dataLayer', 'GTM-5BMDJS2N');
   </script>
   <!-- End Google Tag Manager -->
-  
+
+    <meta name="description"
+          content="Descubra as melhores opções de cartas de consórcio no mercado. Compare e adquira a carta ideal para realizar seus sonhos de forma planejada e segura.">
+    <meta name="keywords"
+          content="cartas de consórcio, consórcio, consórcio imobiliário, consórcio de veículos, carta de crédito, comprar consórcio, melhores cartas de consórcio">
   <!-- Google tag (gtag.js) -->
   <script async src="https://www.googletagmanager.com/gtag/js?id=AW-11312268635"></script>
   <script>
     window.dataLayer = window.dataLayer || [];
-    
+
     function gtag() {
       dataLayer.push(arguments);
     }
     gtag('js', new Date());
-    
+
     gtag('config', 'AW-11312268635');
     </script>
     <!-- End Google Tag -->
