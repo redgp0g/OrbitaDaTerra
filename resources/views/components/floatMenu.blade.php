9435a0c9865d0ac6a3e6b95f696341d868fa1910
redgp0g <guilhermeg2004@gmail.com>
2025-02-18 11:08:20 -0300

    Melhorar a estrutura e legibilidade do menu flutuante e do diálogo, ajustando a formatação e a ordem dos elementos

--- resources/views/components/floatMenu.blade.php
+++ resources/views/components/floatMenu.blade.php
@@ -1,24 +1,30 @@
 <div class="float d-flex align-items-end flex-column-reverse position-fixed bottom-0 end-0 m-5" style="z-index: 1100;">
   <input class="mt-3" type="checkbox" checked />
-  <div class="mt-3 buttonFloatMenu fs-1 d-flex justify-content-center align-items-center"><i class="fa fa-angle-up"></i></div>
+  <div class="buttonFloatMenu fs-1 d-flex justify-content-center align-items-center mt-3"><i class="fa fa-angle-up"></i>
+  </div>
   <div class="floatMenu align-items-end flex-column gap-3">
-    <button class="fs-1 text-white rounded rounded-circle" id="btnmodal" style="background-color: #25d366" target="_blank"
-      onclick="document.getElementById('dialogFaleVendedor').showModal()"><i class="fa fa-whatsapp px-2"></i></button>
-      <button class="fs-5 p-1 text-white rounded rounded-pill" style="background-color: #1e9ef3" onclick="window.location.href = '/simulacao/{{ $cadastro->IDCadastro }}'">Simular Agora</button>
-    <button class="fs-5 bg-success text-white rounded rounded-pill p-1 px-2" onclick="window.location.href = '/carta-a-venda/8'">Venda sua Carta</button>
+    <button class="fs-1 rounded-circle rounded text-white" id="btnmodal" style="background-color: #25d366"
+      onclick="document.getElementById('dialogFaleVendedor').showModal()"><i class="fa fa-whatsapp px-2"></i>
+    </button>
+    <button class="fs-5 rounded-pill rounded p-1 text-white" style="background-color: #1e9ef3"
+      onclick="window.location.href = '/simulacao/{{ $cadastro->IDCadastro }}'">Simular Agora
+    </button>
+    <button class="fs-5 bg-success rounded-pill rounded p-1 px-2 text-white"
+      onclick="window.location.href = '/carta-a-venda/8'">Venda sua Carta
+    </button>
   </div>
 </div>
 
-<dialog class="rounded rounded-4 border p-5" id="dialogFaleVendedor" style="z-index: 100;">
+<dialog class="rounded-4 rounded border p-5" id="dialogFaleVendedor" style="z-index: 100;">
   <span class="modal-close" onclick="dialogFaleVendedor.close()" x-on:click="showModal = false">&times;</span>
   <h2 class="mb-2">Preencha para conversar com um Vendedor</h2>
-  <h4 class="text-center text-danger fs-6">Os campos com * são obrigatórios</h4>
+  <h4 class="text-danger fs-6 text-center">Os campos com * são obrigatórios</h4>
   <form id="enviarDados" method="POST">
     <div class="mb-3">
       <label class="form-label" for="Nome">Nome <span class="text-danger">*</span></label>
       <input class="form-control" id="Nome" name="Nome" type="text" placeholder="Nome" autofocus required />
     </div>
-    <div class="mb-3 d-flex flex-column">
+    <div class="d-flex flex-column mb-3">
       <label class="form-label" for="celular">Celular <span class="text-danger">*</span></label>
       <input class="form-control celular" id="celular" type="text" required />
     </div>
