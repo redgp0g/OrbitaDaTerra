28f25e31ca6c6fa47fb74717a2797b1795a43ab2
redgp0g <guilhermeg2004@gmail.com>
2025-02-18 11:06:15 -0300

    Adicionar detalhes da resposta no console em caso de erro na requisição AJAX

--- resources/views/components/floatMenu.blade.php
+++ resources/views/components/floatMenu.blade.php
@@ -72,6 +72,7 @@
       },
       error: function(jqXHR, textStatus, errorThrown) {
         console.error('Erro na requisição AJAX:', textStatus, errorThrown);
+        console.error('Response details:', jqXHR.responseText);
         alert('Erro na requisição AJAX.');
       }
     });
