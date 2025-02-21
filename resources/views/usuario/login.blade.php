e974c2edb7157592a09b1cd3d3fcb48ea627cf34
redgp0g <guilhermeg2004@gmail.com>
2025-02-20 23:03:48 -0300

    Refatorar layout da página de login para melhorar a estrutura e a legibilidade do código

--- resources/views/usuario/login.blade.php
+++ resources/views/usuario/login.blade.php
@@ -3,11 +3,11 @@
 @section('conteudo')
     <div class="wrapper">
         <section class="login-content">
-            <div class="row m-0 align-items-center bg-white">
+            <div class="row align-items-center m-0 bg-white">
                 <div class="col-md-6">
                     <div class="row justify-content-center">
                         <div class="col-md-10">
-                            <div class="card card-transparent shadow-none d-flex justify-content-center mb-0 auth-card">
+                            <div class="card card-transparent d-flex justify-content-center auth-card mb-0 shadow-none">
                                 <div class="card-body">
                                     <div class="row">
                                         <div class="navbar-brand d-flex align-items-center mb-3">
@@ -18,19 +18,19 @@
                                     <form class="form-group" method="POST" action="{{ route('usuario.auth') }}">
                                         @csrf
                                         <div class="mb-5">
-                                            <label for="login" class="form-label">Email</label>
-                                            <input type="text" class="form-control" id="login" name="Login"
+                                            <label class="form-label" for="login">Email</label>
+                                            <input class="form-control" id="login" name="Login" type="text"
                                                 placeholder="Entre com seu email" autofocus />
                                         </div>
-                                        <div class="mb-3 form-password-toggle">
+                                        <div class="form-password-toggle mb-3">
                                             <div class="d-flex justify-content-between">
                                                 <label class="form-label" for="senha">Senha</label>
                                             </div>
                                             <div class="input-group input-group-merge">
-                                                <input type="password" id="senha" class="form-control" name="Senha"
-                                                    placeholder="Senha" aria-describedby="password" />
+                                                <input class="form-control" id="senha" name="Senha" type="password"
+                                                    aria-describedby="password" placeholder="Senha" />
                                             </div>
-                                            <div class="d-flex flex-row-reverse m-2">
+                                            <div class="d-flex m-2 flex-row-reverse">
                                                 <a href="{{ route('usuario.recoverPassword') }}">Esqueceu a Senha?</a>
                                             </div>
                                         </div>
@@ -52,8 +52,8 @@
                         </div>
                     </div>
                 </div>
-                <div class="col-md-6 d-md-block d-none bg-primary p-0 overflow-hidden" style="height: 95vh;">
-                    <img src="{{ asset('/images/auth/01.png') }}" class="img-fluid gradient-main animated-scaleX"
+                <div class="col-md-6 d-md-block d-none bg-primary overflow-hidden p-0" style="height: 95vh;">
+                    <img class="img-fluid gradient-main animated-scaleX" src="{{ asset('/images/auth/01.png') }}"
                         alt="images">
                 </div>
             </div>
